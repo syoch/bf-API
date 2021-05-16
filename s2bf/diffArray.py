faa32594ba72eac2ce67bdaca1e2a1046ad65aac
syoch <hacksyoch@gmail.com>
2021-05-16 21:33:33 +0900

    update: add diffArray

--- s2bf/diffArray.py
+++ s2bf/diffArray.py
@@ -1,2 +1,12 @@
 def asciiArray(src: str):
     return [ord(ch) for ch in src]
+
+
+def diffArray(src: str):
+    ascArr = asciiArray(src)
+    diff = []
+    val = 0
+    for ch in ascArr:
+        diff.append(ch-val)
+        val = ch
+    return diff
