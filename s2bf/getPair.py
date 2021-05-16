22d5dc04b261170734696a06ebb3aa5842228b47
syoch <hacksyoch@gmail.com>
2021-05-16 22:48:35 +0900

    feat: add offs

--- s2bf/getPair.py
+++ s2bf/getPair.py
@@ -2,14 +2,18 @@ class Pair():
     def __init__(self, a: int, b: int):
         self.a = a
         self.b = b
+        self.offs = 0
+
+    def __str__(self) -> str:
+        return f"({self.a}*{self.b}+{self.offs})"
 
     def get(self):
         if self.a == 1:
-            return self.b
+            return self.b+self.offs
         if self.b == 1:
-            return self.a
+            return self.a+self.offs
 
-        return self.a+self.b+7
+        return self.a+self.b+7+self.offs
 
 
 def getPair(val: int):
