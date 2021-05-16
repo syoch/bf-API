6ba62ccad0508e6c5d71bc679acce5f16c00efeb
syoch <hacksyoch@gmail.com>
2021-05-16 21:45:32 +0900

    update: add getPair

--- s2bf/getPair.py
+++ s2bf/getPair.py
@@ -10,3 +10,18 @@ class Pair():
             return self.a
 
         return self.a+self.b+7
+
+
+def getPair(val: int):
+    pair = Pair(1, val)
+    minscore = val**4
+    minpair = Pair(1, 1)
+    for i in range(1, val+1):
+        if val % i == 0:
+            pair.a = i
+            pair.b = val/i
+            if pair.get() < minscore:
+                minscore = pair.get()
+                minpair.a = pair.a
+                minpair.b = pair.b
+    return minpair
