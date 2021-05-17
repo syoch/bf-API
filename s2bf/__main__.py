24ad4cb2dec6025b48807ae6e9e3e54225fbc86e
syoch <hacksyoch@gmail.com>
2021-05-17 18:51:33 +0900

    update: change loop method

--- s2bf/__main__.py
+++ s2bf/__main__.py
@@ -2,6 +2,7 @@ from . import diffArray, pairGet, remover
 import sys
 src = sys.argv[1]
 out = ""
-for diff in diffArray.diffArray(src):
+for (ch, diff) in zip(*diffArray.diffArray(src)):
+    print(ch, diff)
     out += pairGet.get(diff)+"."
 print(remover.auto(out))
