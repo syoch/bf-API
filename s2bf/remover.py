813b895ec7fd86a5e21a0bd3bf48f79d8207db70
syoch <hacksyoch@gmail.com>
2021-05-18 08:26:57 +0900

    update: add optimize log

--- s2bf/remover.py
+++ s2bf/remover.py
@@ -1,7 +1,9 @@
 def auto(src: str) -> str:
+    _src = src
     oldsrc = ""
     while src != oldsrc:
         oldsrc = src.replace("<>", "")
         oldsrc = oldsrc.replace("><", "")
         src = oldsrc
+    print("optimized: ", len(_src)-len(src), "b")
     return src
