81ad8c051cf18f84fab41d8d239fc1e0f2be406d
syoch <hacksyoch@gmail.com>
2021-05-18 08:45:45 +0900

    update: add peek

--- bfc/parser.py
+++ bfc/parser.py
@@ -3,6 +3,12 @@ class parser():
         self.src = ""
         self.index = 0
 
+    def peek(self):
+        if len(self.src) <= self.index:
+            return None
+        else:
+            return self.src[self.index]
+
     def next(self):
         self.index += 1
         if len(self.src) <= self.index:
