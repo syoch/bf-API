7cfea9510999a4dfcd80b9d6155815bf48ce2d77
syoch <hacksyoch@gmail.com>
2021-05-18 19:22:25 +0900

    bfc: update: impl peek

--- bfc/asm/platform.py
+++ bfc/asm/platform.py
@@ -1,6 +1,7 @@
 table = {
     "pop": ["pop eax"],
     "push": ["push eax"],
+    "peek": ["mov eax, DWORD PTR [esp]"],
     "ins": ["add sp, 1"],
     "des": ["sub sp, 1"],
     "inc": ["add eax,1"],
