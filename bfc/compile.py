8e507f1dd7067336641fafa923d39e6c2357be7c
syoch <hacksyoch@gmail.com>
2021-05-18 09:23:39 +0900

    bfc: update: impl stmts

--- bfc/compile.py
+++ bfc/compile.py
@@ -2,3 +2,15 @@ def compile(ast):
     for stmt in ast:
         if type(stmt) == list:
             raise NotImplementedError()
+        if stmt == "+":
+            print("inc")
+        elif stmt == "-":
+            print("dec")
+        elif stmt == "<":
+            print("lef")
+        elif stmt == ">":
+            print("rig")
+        elif stmt == ".":
+            print("put")
+        elif stmt == ",":
+            print("get")
