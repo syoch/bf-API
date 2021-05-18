769e71557691c37a6ae28e80197a11e8e074b54f
syoch <hacksyoch@gmail.com>
2021-05-18 11:30:16 +0900

    bfc: fix: fix bug of convert.py

--- bfc/asm/convert.py
+++ bfc/asm/convert.py
@@ -6,7 +6,7 @@ def convert(src: List[str]) -> List[str]:
     dest = []
     for line in src:
         if line in table:
-            dest.append(table[line])
+            dest += table[line]
         else:
             dest.append(line)
     return dest
