b1274ee13c038c5715ea1c47472c3edd16fae97a
syoch <hacksyoch@gmail.com>
2021-05-18 09:34:47 +0900

    bfc: update: implement asm.table

--- bfc/asm.py
+++ bfc/asm.py
@@ -1,15 +1,19 @@
 table = {
     "inc": [
-        ""
+        "pop eax",
+        "add eax, 1",
+        "push eax"
     ],
     "dec": [
-
+        "pop eax",
+        "sub eax, 1",
+        "push eax"
     ],
     "lef": [
-
+        "add sp, 1"
     ],
     "rig": [
-
+        "sub sp, 1"
     ],
     "put": [
 
